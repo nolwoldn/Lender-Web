639bcf8c36f57e42bf618f9f54f0f77d6628b92b
Godusop46 <sileshinolan@gmail.com>
2026-07-03 16:47:43 +0300

    Adding admin and before images

--- Lend/admin.py
+++ Lend/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import Error_reporting, Users, Item
 # Register your models here.
+
+
+class ErrorAdmin(admin.ModelAdmin):
+    list_display = ("id", "report", "reporting_usr")
+
+
+class UsrAdmin(admin.ModelAdmin):
+    list_display = ("id", "Name")
+
+
+class ItmAdmin(admin.ModelAdmin):
+    list_display = ("id", "Item_name")
+
+
+admin.site.register(Error_reporting, ErrorAdmin)
+admin.site.register(Users, UsrAdmin)
+admin.site.register(Item, ItmAdmin)
--- Lend/models.py
+++ Lend/models.py
@@ -9,12 +9,17 @@ class Users(models.Model):
     Borrowed_items_id = models.JSONField(default=list, null=True)
     Password = models.CharField(max_length=255, null=True)
 
+    def __str__(self):
+        return f"{self.id} {self.Name}"
+
 
 class Item(models.Model):
     Item_name = models.CharField(max_length=255)
     Item_desc = models.CharField(max_length=1200)
     Item_borrowed_state = models.BooleanField(default=False)
-    
+
+    def __str__(self):
+        return f"{self.id} {self.Item_name}"
 
 
 class Borrow_request(models.Model):
@@ -27,3 +32,6 @@ class Borrow_request(models.Model):
 class Error_reporting(models.Model):
     reporting_usr = models.IntegerField()
     report = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.id} {self.report} {self.reporting_usr}"
